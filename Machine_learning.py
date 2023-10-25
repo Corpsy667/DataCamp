b2ef1bd3c24f72d8467331dda4f1e8b0a27c0ccd
Corpsy667 <damien.lin@efrei.net>
2023-10-25 20:22:48 +0200

    Second commit

--- Machine_learning.py
+++ Machine_learning.py
@@ -8,27 +8,18 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, mean_squared_error
 import matplotlib.pyplot as plt
 from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
 
 def predicting_data_and_fitting(data):
     X_train, X_test, y_train, y_test = train_test_split(data['Sentiment'], data['Popularity'])
     X_train = X_train.to_numpy().reshape(-1, 1)
     X_test = X_test.to_numpy().reshape(-1, 1)
-    linearmodel = KNeighborsClassifier()
+    linearmodel = LogisticRegression()
     linearmodel.fit(X_train, y_train)
     y_pred = linearmodel.predict(X_test)
 
-    # Create a KNN classifier
-    knn = KNeighborsClassifier(n_neighbors=5)  # You can set the number of neighbors (k) here
-
-    # Perform k-fold cross-validation
-    cv_scores = cross_val_score(knn, X_train, y_train, cv=5)  # Replace X and y with your data
-
-    # Print the cross-validation scores
-    print("Cross-validation scores:", cv_scores)
-
-    # Calculate and print the average cross-validation score
-    avg_cv_score = cv_scores.mean()
-    print("Average cross-validation score:", avg_cv_score)
+    mse=mean_squared_error(y_pred, y_test)
+    print("Average cross-validation score:", mse)
 
 
 def plotting_values(data):
