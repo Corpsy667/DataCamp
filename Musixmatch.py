26c5cbeed215b2d3ae153af03147b0333595c240
lionel <lionel22silva@gmail.com>
2023-10-26 16:54:30 +0200

    création du fichier Musixmatch.py, il est censer contenir la fonction pour retrouver les lyrics d'une musique à partir du nom d'un artiste.

--- Musixmatch.py
+++ Musixmatch.py
@@ -1,7 +1,8 @@
 import requests
 
 # Remplacez YOUR_API_KEY par votre clé d'API Musixmatch
-api_key = "043ef90b80309d0fd8a8da75d6f9bd24	"
+api_key = "043ef90b80309d0fd8a8da75d6f9bd24"
+
 
 # Recherche de paroles de chanson
 def search_lyrics(track_name, artist_name):
@@ -17,27 +18,29 @@ def search_lyrics(track_name, artist_name):
 
     if response.status_code == 200:
         data = response.json()
-        # Récupérez l'ID de la chanson
-        track_id = data["message"]["body"]["track_list"][0]["track"]["track_id"]
 
-        # Obtenir les paroles de la chanson
-        endpoint = "track.lyrics.get"
-        query_params = {"track_id": track_id, "apikey": api_key}
-        response = requests.get(base_url + endpoint, params=query_params)
+        if "track_list" in data["message"]["body"]:
+            track_list = data["message"]["body"]["track_list"]
+            if track_list:
+                # Récupérez l'ID de la première chanson
+                track_id = track_list[0]["track"]["track_id"]
 
-        if response.status_code == 200:
-            lyrics = response.json()["message"]["body"]["lyrics"]["lyrics_body"]
-            return lyrics
-        else:
-            print("Erreur lors de la récupération des paroles.")
-    else:
-        print("Erreur lors de la recherche de la chanson.")
+                # Obtenir les paroles de la chanson
+                endpoint = "track.lyrics.get"
+                query_params = {"track_id": track_id, "apikey": api_key}
+                response = requests.get(base_url + endpoint, params=query_params)
 
+                if response.status_code == 200:
+                    lyrics = response.json()["message"]["body"]["lyrics"]["lyrics_body"]
+                    return lyrics
+
+    print("Aucun résultat de chanson trouvé ou erreur lors de la recherche.")
     return None
 
+
 # Utilisation de la fonction pour rechercher les paroles d'une chanson
-track_name = "Song Name"
-artist_name = "Artist Name"
+track_name = "HAPPY"
+artist_name = "NF"
 lyrics = search_lyrics(track_name, artist_name)
 
 if lyrics:
