5e5edcafe0ab35820c11bdce8d2e78ec50a16778
Corpsy667 <79983884+Corpsy667@users.noreply.github.com>
2023-10-27 04:03:29 +0200

    Update main.py
    Final commit ?
    
    Signed-off-by: Corpsy667 <79983884+Corpsy667@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,54 +1,54 @@
 import Cleaning_data
-import pandas as pd
+from Analyzing_data import dffeeling
 from textblob import TextBlob
-from Musixmatch import search_lyrics
-from Machine_learning import predicting_data_and_fitting
+import pandas as pd
+from DataCamp.Machine_learning import predicting_data_and_fitting
+from DataCamp.Musixmatch import search_lyrics
+import numpy as np
 
 
-test = Cleaning_data.importing_data()
+def dffeeling(df):
+    user_sentiments = TextBlob(df["Lyrics"].to_string()).sentiment.polarity
 
-def dffeeling(df) :
-    user_sentiments = TextBlob(df["Lyrics"].to_string()).sentiment.polarity 
-    
-        # Comparaison des sentiments
+    # Comparaison des sentiments
     df2 = Cleaning_data.importing_data()
-    df2['Difference'] = abs( df2['Sentiment'] - user_sentiments )
-    
+    print("user sentiments", user_sentiments)
+    df2['Difference'] = abs(df2['Sentiment'] - user_sentiments)
+
     # Sélection des trois chansons avec les sentiments les plus proches de celles saisies par l'utilisateur
     recommended_songs = df2.sort_values(by='Difference').head(3)
-    
-        # Afficher les recommandations à l'utilisateur
-    return (recommended_songs)
-    
 
-#dff = test[test['Name'] == 'Sorry']
-#print(dff)
-#print(dffeeling(dff))
+    # Afficher les recommandations à l'utilisateur
+    return (recommended_songs)
+
+
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
 # Exemple de base de données de musiques (remplacez par votre propre base de données)
 
 data = Cleaning_data.importing_data()
-@app.route('/api/search', methods=['GET'])
+grad = predicting_data_and_fitting(data)
 
+
+@app.route('/api/search', methods=['GET'])
 def search_music():
-    try :
+    try:
         query = " "
         query = request.args.get('query')
         if query is not None:
             # Par exemple, supposez que "database" est votre DataFrame
-            results = data[data['Name'].str.lower().str.startswith(query.lower())]
+            results = data[data['Name'].str.lower().str.startswith(query.lower())][:3]
+
             suggestionsjson = results.to_dict(orient='records')
 
             # Renvoyez les suggestions au format JSON
-            return jsonify(suggestionsjson) , {'Connection': 'keep-alive'}
+            return jsonify(suggestionsjson), {'Connection': 'keep-alive'}
         else:
-        # Gérez le cas où q4
-        # uery est None
-            return jsonify([]) , {'Connection': 'keep-alive'}
+            # Gérez le cas où q4
+            # uery est None
+            return jsonify([]), {'Connection': 'keep-alive'}
 
     except Exception as e:
         app.logger.error(f"Erreur de recherche : {str(e)}")
@@ -57,28 +57,29 @@ def search_music():
 
 @app.route('/api/search-auteur', methods=['GET'])
 def look_auteur():
-    try :
+    try:
         query = " "
         query = request.args.get('query')
         if query is not None:
             # Par exemple, supposez que "database" est votre DataFrame
             results = data[data['Artist'].str.lower().str.startswith(query.lower())][:3]
-            
+
             suggestionsjson = results.to_dict(orient='records')
 
             # Renvoyez les suggestions au format JSON
-            return jsonify(suggestionsjson) , {'Connection': 'keep-alive'}
+            return jsonify(suggestionsjson), {'Connection': 'keep-alive'}
         else:
-        
-            return jsonify([]) , {'Connection': 'keep-alive'}
+
+            return jsonify([]), {'Connection': 'keep-alive'}
 
     except Exception as e:
         app.logger.error(f"Erreur de recherche : {str(e)}")
         return "Erreur interne du serveur", 500
 
+
 @app.route('/api/propose-auteur', methods=['GET'])
 def propose_auteur():
-    try :
+    try:
         query = " "
         query = request.args.get('query')
         if query is not None:
@@ -86,24 +87,25 @@ def propose_auteur():
             artistes = results['Artist'].tolist()
             a = data[data['Artist'].isin(artistes)]
             suggestionsjson = a.to_dict(orient='records')
-            if a is not None :
-                return jsonify(suggestionsjson) 
+            if a is not None:
+                return jsonify(suggestionsjson)
             else:
-                return jsonify([]) , {'Connection': 'keep-alive'}
+                return jsonify([]), {'Connection': 'keep-alive'}
         else:
-            return jsonify([]) , {'Connection': 'keep-alive'}
+            return jsonify([]), {'Connection': 'keep-alive'}
 
     except Exception as e:
         app.logger.error(f"Erreur de recherche : {str(e)}")
         return "Erreur interne du serveur", 500
-    
+
+
 @app.route('/api/propose-feeling', methods=['GET'])
 def propose_mood():
-    try :
+    try:
         query = request.args.get('query')
         if query is not None:
             results = data[data['Name'].str.lower().str.startswith(query.lower())]
-            #results = search_lyrics(query, "Adele")
+            print(results)
             sent = dffeeling(results)
             suggestionsjson = sent.to_dict(orient='records')
             return jsonify(suggestionsjson)
@@ -117,18 +119,18 @@ def propose_mood():
 
 @app.route('/api/pop', methods=['GET'])
 def predict_pop():
-    try :
+    try:
         query = request.args.get('query')
         query2 = request.args.get('query2')
-        if query is not None:
+        if query is not None and query2 is not None:
             results = search_lyrics(query, query2)
-            grad = predicting_data_and_fitting(data)
-
-            user_sentiment = TextBlob(results.sentiment.polarity)
+            res = pd.DataFrame({'Sentiment': [results]})
+            user_sentiment = TextBlob(res['Sentiment'].values[0]).sentiment.polarity
+            user_sentiment = np.array(user_sentiment).reshape(1, -1)
             user_pop = grad.predict(user_sentiment)
-        
-            data['Difference_pop'] = abs( data['Popularity'] - user_pop )
-            
+
+            data['Difference_pop'] = abs(data['Popularity'] - user_pop)
+
             # Sélection des trois chansons avec les sentiments les plus proches de celles saisies par l'utilisateur
             reco = data.sort_values(by='Difference_pop').head(3)
 
@@ -140,6 +142,7 @@ def predict_pop():
     except Exception as e:
         app.logger.error(f"Erreur de recherche : {str(e)}")
         return "Erreur interne du serveur", 500
+
 
 if __name__ == "__main__":
     app.run()
